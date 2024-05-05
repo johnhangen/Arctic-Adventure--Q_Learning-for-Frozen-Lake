92167d52271e1c7cb3c9b846f86137f153362143
johnhangen <jthangen@gmail.com>
2024-05-05 10:53:05 -0400

    Working Q_table visualization

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from src.Q_learning import Q_learning
 
 
 def main() -> None:
-    num_episodes = 1_000_000
+    num_episodes = 500_000
 
     env = FrozenLakeEnv()
     env.init_environment()
@@ -14,19 +14,25 @@ def main() -> None:
 
     # TODO: it would make sense to add some convergence criteria
     for ep in range(num_episodes):
+        if ep % 1_000 == 0 and ep != 0:
+            pass
+            #print(f'Episode: {ep}, Epsilon: {agent.epsilon}, Q_table: {agent.Q_table}')
+        
         action = env.get_action_space().sample()
         _ = env.reset()
+
+        agent.decay_epsilon()
 
         state = 0
         while True:
             action = agent.get_action(env, state)
             S_prime, R, _, _, _ = env.step(action)
 
-            threshhold = agent.update_q_table(env.observation, action, R, S_prime)
+            agent.update_q_table(state, action, R, S_prime)
 
             state = S_prime
 
-            if env.terminated or threshhold:
+            if env.terminated:
                 break
     
     agent.save_q_table('data/Q_table.npy')
--- src/Q_learning.py
+++ src/Q_learning.py
@@ -8,20 +8,19 @@ class Q_learning(object):
         #hyperparameters
         self.alpha: float = 0.1
         self.gamma: float = 0.9
-        self.epsilon: float = 0.3
-        self.theta: float = 10e-10
+        self.epsilon: float = 1
+        self.decay: float = 0.99999
+        self.epsilon_min: float = 0.1
+
+    def decay_epsilon(self) -> None:
+        if self.epsilon > self.epsilon_min:
+            self.epsilon = self.epsilon * self.decay
 
     def init_q_table(self, env) -> None:
         self.Q_table = np.zeros((env.get_observation_space().n, env.get_action_space().n))
 
     def update_q_table(self, state:int, action:int, reward:float, next_state:int) -> None:
-        update = self.Q_table[state, action] + self.alpha * (reward + self.gamma * np.max(self.Q_table[next_state]) - self.Q_table[state, action])
         self.Q_table[state, action] = self.Q_table[state, action] + self.alpha * (reward + self.gamma * np.max(self.Q_table[next_state]) - self.Q_table[state, action])
-
-        if np.abs(update - self.Q_table[state, action]) < self.theta:
-            return True
-        else:
-            return False
 
     def get_action(self, env, state:int) -> int:
         if np.random.random() < self.epsilon:
